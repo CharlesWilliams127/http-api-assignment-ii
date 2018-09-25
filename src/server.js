a4eb55a57c8d491d6e4de474010649de994e6ed7
crw4147 <crw4147@rit.edu>
2018-09-25 11:47:12 -0400

    Completed assignment

--- src/server.js
+++ src/server.js
@@ -11,52 +11,46 @@ const urlStruct = {
   '/style.css': htmlHandler.getCSS,
   '/getUsers': jsonHandler.getUsers,
   '/notReal': jsonHandler.notFound,
+  getUsersMeta: jsonHandler.getUsersMeta,
   index: htmlHandler.getIndex,
   notFound: jsonHandler.notFound,
 };
 
 const handlePost = (request, response, parsedUrl) => {
-  //if post is to /addUser (our only POST url)
+  // if post is to /addUser (our only POST url)
   if (parsedUrl.pathname === '/addUser') {
     const res = response;
 
-    //uploads come in as a byte stream that we need 
-    //to reassemble once it's all arrived
     const body = [];
 
-    //if the upload stream errors out, just throw a
-    //a bad request and send it back 
     request.on('error', (err) => {
       console.dir(err);
       res.statusCode = 400;
       res.end();
     });
 
-    //on 'data' is for each byte of data that comes in
-    //from the upload. We will add it to our byte array.
     request.on('data', (chunk) => {
-      body.push(chunk); 
+      body.push(chunk);
     });
 
-    //on end of upload stream. 
+    // on end of upload stream.
     request.on('end', () => {
-      //combine our byte array (using Buffer.concat)
-      //and convert it to a string value (in this instance)
+
       const bodyString = Buffer.concat(body).toString();
-      //since we are getting x-www-form-urlencoded data
-      //the format will be the same as querystrings
-      //Parse the string into an object by field name
+
       const bodyParams = query.parse(bodyString);
 
-      //pass to our addUser function
+      // pass to our addUser function
       jsonHandler.addUser(request, res, bodyParams);
     });
   }
 };
 
-const handleGet = (request, response, parsedUrl) => {
-  //route to correct method based on url
-  if (urlStruct[parsedUrl.pathname]) {
+const handleHeadGet = (request, response, parsedUrl) => {
+  // route to correct method based on url
+  if (parsedUrl.pathname === '/getUsers' && request.method === 'HEAD') {
+    jsonHandler.getUsersMeta(request, response);
+  } else if (urlStruct[parsedUrl.pathname]) {
     urlStruct[parsedUrl.pathname](request, response);
   } else {
     urlStruct.notFound(request, response);
@@ -69,7 +63,7 @@ const onRequest = (request, response) => {
   if (request.method === 'POST') {
     handlePost(request, response, parsedUrl);
   } else {
-    handleGet(request, response, parsedUrl);
+    handleHeadGet(request, response, parsedUrl);
   }
 };
 
